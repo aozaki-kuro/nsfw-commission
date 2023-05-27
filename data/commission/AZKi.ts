ad8eab6981c39251b522126c140440281e841918
Aozaki Kuro <i@aozaki.cc>
2023-05-27 18:32:37 +0800

    update: new commission record

--- data/commission/AZKi.ts
+++ data/commission/AZKi.ts
@@ -10,5 +10,12 @@ export const AZKi: Props[] = [
     Twitter: 'https://twitter.com/so_dasui1/status/1660644177183404035',
     Pixiv: '',
     Skeb: ''
+  },
+  {
+    fileName: '20230527_病ん太廊',
+    Character: 'azki',
+    Twitter: '',
+    Pixiv: '',
+    Skeb: 'https://skeb.jp/@hcz_n/works/50'
   }
 ]
