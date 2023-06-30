bb586f70717529eda5090b05515f48f3b6bec02d
Aozaki Kuro <i@aozaki.cc>
2023-07-01 07:25:21 +0800

    chore: remove `useMemo`

--- components/commission/listing.tsx
+++ components/commission/listing.tsx
@@ -1,6 +1,5 @@
 import { commissionData } from '#data/CommissionData'
 import Image from 'next/image'
-import { useMemo } from 'react'
 import IllustratorInfo from './illustrator-info'
 import { CommissionInfoProps } from './types'
 
@@ -21,16 +20,15 @@ const renderCommission = (commission: CommissionInfoProps) => (
 )
 
 const Listing = ({ Character }: Props) => {
-  const commissions = useMemo(() => {
-    const lowercaseCharacter = Character.toLowerCase()
-    return Object.values(commissionData)
-      .filter(commission => commission.Character.toLowerCase() === lowercaseCharacter)
-      .map(commission => {
-        const [PublishDate, Creator] = commission.fileName.split('_')
-        return { ...commission, PublishDate: PublishDate.slice(0, 8), Creator }
-      })
-      .sort((a, b) => b.PublishDate.localeCompare(a.PublishDate))
-  }, [Character])
+  const lowercaseCharacter = Character.toLowerCase()
+  const commissions = Object.values(commissionData)
+    .filter(commission => commission.Character.toLowerCase() === lowercaseCharacter)
+    .map(commission => ({
+      ...commission,
+      PublishDate: commission.fileName.slice(0, 8),
+      Creator: commission.fileName.split('_')[1]
+    }))
+    .sort((a, b) => b.PublishDate.localeCompare(a.PublishDate))
 
   if (commissions.length === 0) {
     return <p>To be announced ...</p>
--- components/commission/update.tsx
+++ components/commission/update.tsx
@@ -1,7 +1,6 @@
 import { charaDictionary } from '#data/CharaDictionary'
 import { commissionData } from '#data/CommissionData'
 import Link from 'next/link'
-import { useMemo } from 'react'
 
 type LatestEntry = {
   fileName: string
@@ -14,27 +13,20 @@ const kebabCase = (str: string) =>
     .replace(/'/g, '')
     .replace(/[\W_]+/g, '-')
 
-// Calculate the latest entry outside of the component, since commissionData is static
-const latestEntry: LatestEntry | null = Object.values(commissionData).reduce(
-  (latest: LatestEntry | null, { fileName, Character }) => {
-    const publishDateInt = parseInt(fileName.substring(0, 8))
-
-    if (isNaN(publishDateInt)) return latest
-
-    if (!latest || latest.fileName < fileName) {
-      return { fileName, Character }
-    }
-
-    return latest
-  },
-  null
-)
-
 const Update = () => {
-  const character = latestEntry ? latestEntry.Character : ''
-  const dictionaryEntry = useMemo(
-    () => charaDictionary.find(chara => chara.Abbr === character),
-    [character]
+  const latestEntry = Object.values(commissionData).reduce(
+    (latest: LatestEntry | null, { fileName, Character }) => {
+      const publishDateInt = parseInt(fileName.substring(0, 8))
+
+      if (isNaN(publishDateInt)) return latest
+
+      if (!latest || latest.fileName < fileName) {
+        return { fileName, Character }
+      }
+
+      return latest
+    },
+    null
   )
 
   if (!latestEntry) {
@@ -45,8 +37,9 @@ const Update = () => {
     4,
     6
   )}/${latestEntry.fileName.substring(6, 8)}`
-
-  const fullName = dictionaryEntry?.FullName ?? latestEntry.Character.toLowerCase()
+  const { Character } = latestEntry
+  const dictionaryEntry = charaDictionary.filter(chara => chara.Abbr === Character)[0]
+  const fullName = dictionaryEntry?.FullName ?? Character.toLowerCase()
 
   return (
     <div className="flex flex-auto font-mono text-sm ss:text-xs">
