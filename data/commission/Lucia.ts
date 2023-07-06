673bc4ef4b8eef427501c0b523aff5e778f677a4
Aozaki Kuro <i@aozaki.cc>
2023-07-06 23:26:56 +0800

    update: new commission

--- data/commission/Lucia.ts
+++ data/commission/Lucia.ts
@@ -1,14 +1,23 @@
 import { Props } from '#data/types'
 
+const currentChara = 'lucia'
+
 export const Lucia: Props[] = [
   /*
    * Exsisting "Lucia"
    */
   {
     fileName: '20230617_温野りょく',
-    Character: 'lucia',
+    Character: currentChara,
     Twitter: 'https://twitter.com/UnoRyoku/status/1671030802241716231',
     Pixiv: 'https://www.pixiv.net/artworks/109181457',
     Skeb: 'https://skeb.jp/@UnoRyoku/works/133',
+  },
+  {
+    fileName: '20230706_七市',
+    Character: currentChara,
+    Twitter: '',
+    Pixiv: '',
+    Skeb: 'https://skeb.jp/@nanashi_nogi/works/263',
   },
 ]
