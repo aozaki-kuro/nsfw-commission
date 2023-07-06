1e49d1cde837f087129a119099faa3b6ed0e52d8
Aozaki Kuro <i@aozaki.cc>
2023-07-06 23:27:17 +0800

    fix: simplify character name input

--- data/commission/AZKi.ts
+++ data/commission/AZKi.ts
@@ -1,40 +1,42 @@
 import { Props } from '#data/types'
 
+const currentChara = 'azki'
+
 export const AZKi: Props[] = [
   /*
    * Exsisting "AZKi"
    */
   {
     fileName: '20230515_そーだみず',
-    Character: 'azki',
+    Character: currentChara,
     Twitter: 'https://twitter.com/so_dasui1/status/1660644177183404035',
     Pixiv: '',
     Skeb: '',
   },
   {
     fileName: '20230527_病ん太廊',
-    Character: 'azki',
+    Character: currentChara,
     Twitter: 'https://twitter.com/hcz_n/status/1663893329963286533',
     Pixiv: 'https://www.fanbox.cc/@hv-yantaro/posts/6073423',
     Skeb: 'https://skeb.jp/@hcz_n/works/50',
   },
   {
     fileName: '20230603_うたゆうか',
-    Character: 'azki',
+    Character: currentChara,
     Twitter: 'https://twitter.com/YuukaUta/status/1665328780427296770',
     Pixiv: 'https://www.pixiv.net/artworks/108727689',
     Skeb: 'https://skeb.jp/@YuukaUta/works/79',
   },
   {
     fileName: '20230611_病ん太廊',
-    Character: 'azki',
+    Character: currentChara,
     Twitter: '',
     Pixiv: '',
     Skeb: 'https://skeb.jp/@hcz_n/works/51',
   },
   {
     fileName: '20230702_アータ',
-    Character: 'azki',
+    Character: currentChara,
     Twitter: 'https://twitter.com/C4Rm93/status/1669365839303434240',
     Pixiv: 'https://www.pixiv.net/artworks/109525980',
     Skeb: '',
--- data/commission/Misc.ts
+++ data/commission/Misc.ts
@@ -1,12 +1,14 @@
 import { Props } from '#data/types'
 
+const currentChara = 'misc'
+
 export const Misc: Props[] = [
   /*
    * Other characters
    */
   {
     fileName: '20230613_中村六翅',
-    Character: 'misc',
+    Character: currentChara,
     Twitter: 'https://twitter.com/NakamuraRohane/status/1668625063602708483',
     Pixiv: 'https://www.pixiv.net/artworks/108990302',
     Skeb: '',
