ae30e4091cda46f06ae36685c928b224ccb0be01
Aozaki Kuro <i@aozaki.cc>
2023-07-09 17:07:55 +0800

    chore: reserve dict entry

--- data/CharaDictionary.ts
+++ data/CharaDictionary.ts
@@ -6,9 +6,7 @@ interface Chara {
 export const charaDictionary: Chara[] = [
   { Abbr: 'azki', FullName: 'AZKi' },
   { Abbr: 'lucia', FullName: 'Lucia' },
-  {
-    Abbr: 'ina',
-    FullName: "Ninomae Ina'Nis",
-  },
+  { Abbr: 'ina', FullName: "Ninomae Ina'Nis" }, // Too long, fix with comment lol
   { Abbr: 'misc', FullName: 'Misc' },
+  { Abbr: 'nayuta', FullName: 'nayuta' }, // Could change to La prière
 ]
--- data/CommissionData.ts
+++ data/CommissionData.ts
@@ -2,27 +2,26 @@ import { Props } from '#data/types'
 
 // Active
 import { AZKi } from '#data/commission/AZKi'
-import { Ina } from '#data/commission/Ina'
 import { Lucia } from '#data/commission/Lucia'
 
 // Misc
 import { Misc } from './commission/Misc'
 
 // Stale
+import { Ina } from '#data/commission/stale/Ina'
 import { Nishe } from '#data/commission/stale/Nishe'
 import { Tkmt } from '#data/commission/stale/Tkmt'
 // import { nayuta } from './commission/Nayuta'
 
 export const commissionData: Props[] = [
-  //Active Data
+  /* ===== Active data ===== */
   ...AZKi,
   ...Lucia,
-  // ...nayuta,
+  // ...nayuta, // Avoid import null or could error
 
-  // Misc
   ...Misc,
 
-  //Stale Data
+  /* ===== Stale data ===== */
   ...Ina,
   ...Nishe,
   ...Tkmt,
